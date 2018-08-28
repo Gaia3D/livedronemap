22a2d75110a65d363c03100f424db28c8ee67212
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2018-08-28 17:38:04 +0900

    temp

--- livedronemap-doc/database/ddl/sequence/sequence.sql
+++
@@ -1,7 +0,0 @@
-drop sequence if exists policy_seq;
-drop sequence if exists file_info_seq;
-drop sequence if exists upload_log_seq;
-
-create sequence policy_seq increment 1 minvalue 1 maxvalue 999999999999 start 2 cache 1;
-create sequence file_info_seq increment 1 minvalue 1 maxvalue 999999999999 start 1 cache 1;
-create sequence upload_log_seq increment 1 minvalue 1 maxvalue 999999999999 start 1 cache 1;
