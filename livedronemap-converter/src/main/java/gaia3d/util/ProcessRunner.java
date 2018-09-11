705abe5595cac1fdf4d2a3582152956d7cf12bf7
jskim <jskim@gaia3d.com>
2018-09-11 16:09:13 +0900

    명령어 실행 클레스 추가

---
+++ livedronemap-converter/src/main/java/gaia3d/util/ProcessRunner.java
@@ -0,0 +1,18 @@
+package gaia3d.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 외부 명령 실행 
+ * @author jskim
+ *
+ */
+@Component
+public class ProcessRunner {
+	
+	// TODO cmd 명령 실행
+	public void exexProcess() {
+		
+	}
+	
+}
