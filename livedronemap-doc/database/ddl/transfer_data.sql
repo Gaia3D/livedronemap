240c74737f997cc394730801bfac63374f38108d
jinwoo park <jwpark@gaia3d.com>
2018-09-11 21:30:33 +0900

    ddl 변경

--- livedronemap-doc/database/ddl/transfer_data.sql
+++ livedronemap-doc/database/ddl/transfer_data.sql
@@ -1,14 +1,25 @@
+-- FK, Index는 별도 파일로 분리.
 drop table if exists transfer_data cascade;
 
 -- 전송된 데이터 
 create table transfer_data(
-	transfer_data_id					bigint,
-	proejct_id							bigint,
-	
-	constraint transfer_data_id_pk		primary key (transfer_data_id),
-	constraint project_id_fk 			foreign key (proejct_id)
+	transfer_data_id				bigint,
+	proejct_id						bigint,				not null,
+	image_type						char(1),
+	orthoretified_image_count		bigint				default 0,
+	processing_image_count			bigint				default 0,
+	total_image_count				bigint				default 0,
+	transfer_sucessyn				char(1),
+	image_type						char(1),
+	constraint transfer_data_id_pk	primary key (transfer_data_id),
 );
 
 comment on table transfer_data is '전송된 데이터 관리';
 comment on column transfer_data.transfer_data_id is 'transfer_data 고유번호';
 comment on column transfer_data.proejct_id is 'project 고유번호';
+comment on column transfer_data.orthoretified_image_count is '개별정사영상 갯수';
+comment on column transfer_data.processing_image_count is '후처리영상 갯수';
+comment on column transfer_data.total_image_count is '전체 영상의 갯수';
+comment on column transfer_data.transfer_time is '전송 시간';
+comment on column transfer_data.transfer_sucessyn is '데이터 전송 성공 유무 ( Y : 성공, N : 실패 )';
+comment on column transfer_date.image_type is '데이터 구분 ( 0 : 개별정사영상, 1 : 후처리영상)
