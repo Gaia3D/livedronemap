12f4a6017169644b0ba42e1d27e52a1dfee77f3b
jskim <jskim@gaia3d.com>
2018-09-11 19:13:28 +0900

    좌표 변환, 내부 타일 생성, 오버뷰 생성 추가

--- livedronemap-converter/src/main/java/gaia3d/util/ImageConvertUtil.java
+++ livedronemap-converter/src/main/java/gaia3d/util/ImageConvertUtil.java
@@ -1,6 +1,17 @@
 package gaia3d.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 이미지 변환 using GDAL Command
@@ -8,7 +19,54 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@Slf4j
 public class ImageConvertUtil {
+	
+	@Value("${gdal.cmd.path}")
+	private String gdalCmdPath;
+	
+	@Autowired
+	private ProcessRunner processRunner;
+	
+	/**
+	 * Mago3D에서 서비스하는 EPSG:4326로 변환 
+	 * @param srcImg 변환할 이미지 경로 
+	 * @param targetImg 저장할 이미지 경로 
+	 * @param srcSrs 젼환할 이미지 좌표계 
+	 * @param targetSrs 저장할 이미지 좌표계 
+	 * @param cmdOpt 기타 gdalwarp 옵션. 참고 : https://www.gdal.org/gdalwarp.html 
+	 */
+	public void convertProjection(
+			String srcImg, String targetImg, String srcSrs, String targetSrs, List<String> cmdOpt) {
+		log.info("Start gdalwarp .. {}", srcImg);
+		checkFileExists(targetImg);
+		
+		List<String> cmdList = new ArrayList<>();
+		
+		Path cmdPath = Paths.get(gdalCmdPath, "gdalwarp");
+		
+		cmdList.add(cmdPath.toString());
+		
+		if (cmdOpt != null) {
+			cmdList.addAll(cmdOpt);
+		}
+		
+		cmdList.add("-s_srs");
+		cmdList.add(srcSrs);
+		
+		cmdList.add("-t_srs");
+		cmdList.add(targetSrs);
+		
+		cmdList.add(srcImg);
+		cmdList.add(targetImg);
+		
+		try {
+			processRunner.execProcess(cmdList);
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
 	// TODO 배경제거
 	/**
@@ -18,27 +76,79 @@ public class ImageConvertUtil {
 		
 	}
 	
-	// TODO 좌표 변환
 	/**
-	 * Mago3D에서 서비스하는 EPSG:4326로 변환 
+	 * 내부 타일 생성 
+	 * @param srcImg 변환할 이미지 경로 
+	 * @param targetImg 저장할 이미지 경로 
+	 * @param cmdOpt 기타 gdal_translate 옵션. 참고 : https://www.gdal.org/gdal_translate.html
 	 */
-	public void convertProjection() {
+	public void createInnerTile(String srcImg, String targetImg, List<String> cmdOpt) {
+		log.info("Start gdal_translate .. {}", srcImg);
+		
+		checkFileExists(targetImg);
+		
+		List<String> cmdList = new ArrayList<>();
+		
+		Path cmdPath = Paths.get(gdalCmdPath, "gdal_translate");
+		
+		cmdList.add(cmdPath.toString());
+		
+		if (cmdOpt != null) {
+			cmdList.addAll(cmdOpt);
+		}
+		
+		cmdList.add(srcImg);
+		cmdList.add(targetImg);
+		
+		try {
+			processRunner.execProcess(cmdList);
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	// TODO 내부타일생성
-	/**
-	 * 내부 타일 생성
-	 */
-	public void createInnerTile() {
-		
-	}
-	
-	// TODO 오버뷰 생성
 	/**
 	 * 오버뷰 생성 
+	 * @param srcImg 변환할 이미지 경로 
+	 * @param overviewLevel 
+	 * @param cmdOpt 기타 gdaladdo 옵션. 참고 : https://www.gdal.org/gdaladdo.html
 	 */
-	public void createOverview() {
+	public void createOverview(String srcImg, int overviewLevel, List<String> cmdOpt) {
+		log.info("Start gdaladdo .. {}", srcImg);
+		
+		List<String> cmdList = new ArrayList<>();
+		
+		Path cmdPath = Paths.get(gdalCmdPath, "gdaladdo");
+		
+		cmdList.add(cmdPath.toString());
+		
+		if (cmdOpt != null) {
+			cmdList.addAll(cmdOpt);
+		}
+		
+		cmdList.add(srcImg);
+		
+		int initLevel = 2;
+		for (int i = 1; i <= overviewLevel; i++) {
+			cmdList.add(String.valueOf(initLevel));
+			initLevel *= 2;
+		}
+		
+		try {
+			processRunner.execProcess(cmdList);
+		} catch (InterruptedException | IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void checkFileExists(String path) {
+		File file = new File(path);
+		
+		if (file.exists()) {
+			file.delete();
+		}
 		
 	}
 	
