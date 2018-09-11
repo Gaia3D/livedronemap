2fa293356817d09be4f78b10ee2c13c99bfb2075
jskim <jskim@gaia3d.com>
2018-09-11 19:11:52 +0900

    cmd 명령 실행 추가

--- livedronemap-converter/src/main/java/gaia3d/util/ProcessRunner.java
+++ livedronemap-converter/src/main/java/gaia3d/util/ProcessRunner.java
@@ -1,6 +1,14 @@
 package gaia3d.util;
 
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 외부 명령 실행 
@@ -8,11 +16,30 @@ import org.springframework.stereotype.Component;
  *
  */
 @Component
+@Slf4j
 public class ProcessRunner {
 	
-	// TODO cmd 명령 실행
-	public void exexProcess() {
-		
+	ProcessBuilder builder = new ProcessBuilder();
+	
+	@PostConstruct
+	public void printCommandStatus() {
+		// 실행하는 명령어의 프린트 출력을 그대로 전달 도록 설정;
+		this.builder.redirectOutput(Redirect.INHERIT); 
+		this.builder.redirectError(Redirect.INHERIT);
+	}
+	
+	/**
+	 * CLI 명령을 String배열로 받아서 실행 
+	 * 
+	 * @param command
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	public void execProcess(List<String> command) throws InterruptedException, IOException  {
+		log.debug("Exec command : {}", String.join(" ", command));
+		builder.command(command);
+	    Process process = builder.start();
+		process.waitFor();
 	}
 	
 }
