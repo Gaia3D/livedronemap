dbd06f2fb2d835746603739019858a988db853f8
jinwoo park <jwpark@gaia3d.com>
2018-09-12 21:59:28 +0900

    ddl 수정

--- livedronemap-doc/database/ddl/processing_image_file.sql
+++
@@ -1,6 +0,0 @@
--- FK, Index는 별도 파일로 분리.
-drop table if exists processing_image_file cascade;
-
-create table orthoretified_image_file(
-	
-);
--- livedronemap-doc/database/ddl/processing_image_log.sql
+++
@@ -1,6 +0,0 @@
--- FK, Index는 별도 파일로 분리.
-drop table if exists processing_image_log cascade;
-
-create table orthoretified_image_log(
-	
-);
