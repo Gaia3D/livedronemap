6eceaa5933ef6385591039d06614ff41375d0349
jskim <jskim@gaia3d.com>
2018-09-12 15:29:53 +0900

    Test 수정

--- livedronemap-converter/src/test/java/gaia3d/util/ImageConvertUtilTest.java
+++ livedronemap-converter/src/test/java/gaia3d/util/ImageConvertUtilTest.java
@@ -1,74 +1,65 @@
 package gaia3d.util;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.FixMethodOrder;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import gaia3d.config.GdalConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@TestPropertySource("classpath:gdal.properties")
+//@TestPropertySource("classpath:gdal.properties")
 public class ImageConvertUtilTest {
 	
 	@Autowired
-	ImageConvertUtil imageConvertUtil;
+	GdalConfig gdalConfig;
 	
 	@Test
+	@Ignore
 	public void test000ConvertProjection() throws InterruptedException, IOException {
-		String srcImg = "/Volumes/macDATA/source/2018/livedronemap/livedronemap-converter/src/test/resources/img/DJI_0064.png";
-		String targetImg = "/Volumes/macDATA/source/2018/livedronemap/livedronemap-converter/src/test/resources/img/result/DJI_0064.tif";
-		String srcSrs = "EPSG:4326";
-		String targetSrs = "EPSG:4326";
-		List<String> cmdOpt = new ArrayList<>();
-		
-		// output 포맷 
-		cmdOpt.add("-of");
-		cmdOpt.add("GTiff");
-		
-		// 리샘플링 알고리즘 
-		cmdOpt.add("-r");
-		cmdOpt.add("cubic");
-		
-		imageConvertUtil.convertProjection(srcImg, targetImg, srcSrs, targetSrs, cmdOpt);
+		Path targetPath = Paths.get("src", "test", "resources", "img", "DJI_0064.png");
+		String srcImg = targetPath.toAbsolutePath().toString();
+		ImageConvertUtil imageConvertUtil = new ImageConvertUtil(gdalConfig, srcImg);
+		imageConvertUtil.convertProjection(srcImg);
 	}
 	
 	@Test
+	@Ignore
 	public void test001CreateInnerTile() throws InterruptedException, IOException {
-		String srcImg = "/Volumes/macDATA/source/2018/livedronemap/livedronemap-converter/src/test/resources/img/result/DJI_0064.tif";
-		String targetImg = "/Volumes/macDATA/source/2018/livedronemap/livedronemap-converter/src/test/resources/img/result/DJI_0064_tiled.tif";
-		List<String> cmdOpt = new ArrayList<>();
-		
-		// output 포맷 
-		cmdOpt.add("-of");
-		cmdOpt.add("GTiff");
-		
-		// 타일 생성 여부  
-		cmdOpt.add("-co");
-		cmdOpt.add("TILED=YES");
-		
-		imageConvertUtil.createInnerTile(srcImg, targetImg, cmdOpt);
+		Path targetPath = Paths.get("src", "test", "resources", "img", "DJI_0064_warp.tif");
+		String srcImg = targetPath.toAbsolutePath().toString();
+		ImageConvertUtil imageConvertUtil = new ImageConvertUtil(gdalConfig, srcImg);
+		imageConvertUtil.createInnerTile(srcImg);
 	}
 	
 	@Test
-	public void test002C() throws InterruptedException, IOException {
-		String srcImg = "/Volumes/macDATA/source/2018/livedronemap/livedronemap-converter/src/test/resources/img/result/DJI_0064_tiled.tif";
-		int overviewLevel = 7;
-		List<String> cmdOpt = new ArrayList<>();
+	@Ignore
+	public void test002CreateOverview() throws InterruptedException, IOException {
+		Path targetPath = Paths.get("src", "test", "resources", "img", "DJI_0064_warp_tiled.tif");
+		String srcImg = targetPath.toAbsolutePath().toString();
+		ImageConvertUtil imageConvertUtil = new ImageConvertUtil(gdalConfig, srcImg);
+		imageConvertUtil.createOverview(srcImg);
+	}
+	
+	@Test
+	public void test003() throws InterruptedException {
+		Path targetPath = Paths.get("src", "test", "resources", "img", "DJI_0064.png");
+		String srcImg = targetPath.toAbsolutePath().toString();
+		Runnable imageConvertUtil = new ImageConvertUtil(gdalConfig, srcImg);
+		Thread thread = new Thread(imageConvertUtil);
+		thread.start();
 		
-		// 리샘플링 알고리즘 
-		cmdOpt.add("-r");
-		cmdOpt.add("average");
-		
-		imageConvertUtil.createOverview(srcImg, overviewLevel, cmdOpt);
+		thread.join();
 	}
 
 }
