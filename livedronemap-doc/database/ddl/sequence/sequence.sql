099b7747773938783ab692b948da7d4c9509de89
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2018-09-14 10:17:11 +0900

    1차 완료

--- livedronemap-doc/database/ddl/sequence/sequence.sql
+++ livedronemap-doc/database/ddl/sequence/sequence.sql
@@ -1,5 +1,7 @@
-drop sequence if exists drone_project_seq;
 drop sequence if exists drone_seq;
+drop sequence if exists drone_project_seq;
+
+
 
 create sequence drone_seq increment 1 minvalue 1 maxvalue 999999999 start 1 cache 1;
 create sequence drone_project_seq increment 1 minvalue 1 maxvalue 999999999 start 1 cache 1;
