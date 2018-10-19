15cf42724d90099f60f774d6be01bd15415b050a
jskim <jskim@gaia3d.com>
2018-10-19 15:53:07 +0900

    페이지 이동 임시 처리

--- livedronemap-admin/src/main/webapp/js/live-drone-map.js
+++ livedronemap-admin/src/main/webapp/js/live-drone-map.js
@@ -15,24 +15,29 @@ function cameraFlyTo(longitude, latitude, altitude, duration) {
 
 //url 페이지로 이동
 function goPage(url) {
-	if(url === '/drone-project/list-drone-project') {
-		var currentUrl = location.href;
-		if(currentUrl.indexOf(url) >= 0) {
-			console.log("내 페이지에서 나를 누름");
-			// 자기 페이지에서 자기 메뉴를 누름
-			if($("#leftMenuArea").css("display") == "none") {
-				$("#leftMenuArea").show();
-				$(".mapWrap").css({"padding-left" : "391px"});
+	if(url === location.pathname) {
+		if (url === '/drone-project/list-drone-project') {
+			var currentUrl = location.href;
+			if(currentUrl.indexOf(url) >= 0) {
+				console.log("내 페이지에서 나를 누름");
+				// 자기 페이지에서 자기 메뉴를 누름
+				if($("#leftMenuArea").css("display") == "none") {
+					$("#leftMenuArea").show();
+					$(".mapWrap").css({"padding-left" : "391px"});
+				} else {
+					$("#leftMenuArea").hide();
+					$(".mapWrap").css({"padding-left" : "51px"});
+				}
+				return;
 			} else {
-				$("#leftMenuArea").hide();
-				$(".mapWrap").css({"padding-left" : "51px"});
+				console.log("다른 페이지에서 나를 누름");
+				location.href = url;
+				return;
 			}
-			return;
 		} else {
-			console.log("다른 페이지에서 나를 누름");
-			location.href = url;
 			return;
 		}
+		
 	} else {
 		// 다른 페이지로 이동
 		location.href = url;
