88b61f456256a096019f358ade3f888fa9fcea91
DESKTOP-49U0CRN\Cheon JeongDae <jdcheon@gaia3d.com>
2018-11-01 10:52:29 +0900

    bug fix

--- livedronemap-admin/src/main/java/gaia3d/domain/DroneProject.java
+++ livedronemap-admin/src/main/java/gaia3d/domain/DroneProject.java
@@ -15,7 +15,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class DroneProject extends SearchFilter {
+public class DroneProject extends SearchDomain {
 	
 	/****** validator ********/
 	private String method_mode;
